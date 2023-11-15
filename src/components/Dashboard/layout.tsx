ee91175d8085fcc101a6362e6323cd4d60046245
varchodi <rauschenrobert@gmail.com>
2023-11-15 22:49:24 +0300

    delayed theme change

--- src/components/Dashboard/layout.tsx
+++ src/components/Dashboard/layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ children }: {
 }) => {
   const [showNav, setSetshowNav] = useState<boolean>(false)
   return (
-      <div className='w-full h-screen p-2 flex flex-row gap-4 bg-gray-200 dark:bg-slate-950 dark:text-slate-100 md:p-10 xl:p-20 2xl:p-24'>
+      <div className='w-full h-screen p-2 flex flex-row gap-4 bg-gray-200 dark:bg-slate-950 dark:text-slate-100 md:p-10 xl:p-20 2xl:p-24 duration-700 delay-300'>
           {/* navbar */}
           <Navbar setShow={setSetshowNav} isShown={showNav} />
 
